a5af6c0c50ca016daad9bc0da644b0eb21ab45da
Anshika Singh <20anshika.singh10b@gmail.com>
2025-03-22 15:23:25 +0530

    removed files

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-// frontend/vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Vite automatically detects PostCSS config, no need to specify it manually
-export default defineConfig({
-  plugins: [react()],
-});
